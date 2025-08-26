92efbf5b7de81556ab9cb47ab92790f6eef232a2
GEOVANNA CAROLINE CAMPOS LIMA <geovanna_c_lima@estudante.sesisenai.org.br>
2025-08-25 22:18:03 -0300

    AULA 13 E ATV 13

---
+++ atividades/aula13/atv13.js
@@ -0,0 +1,66 @@
+//Lista de atividades
+
+//01
+class Usuario{
+  constructor(nome,email,idade){
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+  }
+
+    setNome(nome){
+        this.nome = nome;
+    }
+
+    
+    setNome(email){
+        this.email = email;
+    }
+
+    
+    setNome(idade){
+        this.idade = idade;
+    }
+
+    getNome (){
+        return this.nome;
+    }
+
+    getEmail (){
+        return this.email;
+    }
+
+    getIdade (){
+        return this.idade;
+    }
+
+}
+
+const objUsuario = new Usuario ();
+objUsuario.setNome("Lily");
+objUsuario.getNome()
+
+
+
+//02
+// A) Criando uma classe Funcionario que serve como modelo, permitindo instanciar vários objetos com dados
+// individuais e métodos compartilhados
+
+
+//03
+//
+
+
+//04
+
+
+
+//05
+
+
+
+//06
+
+
+
+//07
